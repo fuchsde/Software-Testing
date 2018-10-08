038dfdec72a3a2eae48ceb95fdc21a3566345dd8
fuchsde <32522107+fuchsde@users.noreply.github.com>
2018-10-08 19:14:38 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,6 +5,10 @@ using std::cout;
 int main (int argc, char *argv[]) 
 { 
   cout << "Hello World\n";
-  cout << "Getting it done";
+  cout << "Getting it done\n";
+  
+  int result = Math.Multiply(2, 3);
+  cout << "Result: " << result;
+  
   return 0;
 } 
