207a754c769b0b0eb2b86d342c6d06165a73be52
fuchsde <32522107+fuchsde@users.noreply.github.com>
2018-10-08 19:25:19 +0300

    Update function.cpp

--- function.cpp
+++ function.cpp
@@ -1,13 +1,1 @@
-include "function.h"
 
-static int Multiply(int a, int b)
-{
-  int result = a * b;
-  return result;
-}
-
-static int Add(int a, int b)
-{
-  int result = a + b;
-  return result;
-}
