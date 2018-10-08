1f8cc04b6b803bb314de9ddb0070699b94612013
fuchsde <32522107+fuchsde@users.noreply.github.com>
2018-10-08 19:29:06 +0300

    Update function.h

--- function.h
+++ function.h
@@ -4,5 +4,12 @@ using namespace std;
 
 static int Multiply(int a, int b)
 {
-  return 0;
+  int result = a * b;
+  return result;
+};
+
+static int Add(int a, int b)
+{
+  int result = a + b;
+  return result;
 };
