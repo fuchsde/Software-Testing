1048b0f46e24ab3a65eebc83ab70d243803ca937
fuchsde <32522107+fuchsde@users.noreply.github.com>
2018-10-08 19:50:47 +0300

    Update testAdd.cpp

--- testAdd.cpp
+++ testAdd.cpp
@@ -11,10 +11,10 @@ int main (int argc, char *argv[])
       for(int j = 0; j < 10; j++)
       {
           result = Add(i, j);
-          cout << "i: " << i << " + j: " << j << " = " << result; 
-          if(result != i*j)
+          cout << "i: " << i << " + j: " << j << " = " << result << "\n"; 
+          if(result != i + j)
           {
-            cout << "Testing add failed";
+            cout << "Testing add failed\n";
           }
       }
     }
