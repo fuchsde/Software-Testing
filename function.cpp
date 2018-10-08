3e7df9369cff15eebc7dac98e8f581155c209965
fuchsde <32522107+fuchsde@users.noreply.github.com>
2018-10-08 19:22:51 +0300

    Update function.cpp

--- function.cpp
+++ function.cpp
@@ -1,12 +1,12 @@
 include <function.h>
 
-int Multiply(int a, int b)
+static int Multiply(int a, int b)
 {
   int result = a * b;
   return result;
 }
 
-Add(int a, int b)
+static int Add(int a, int b)
 {
   int result = a + b;
   return result;
