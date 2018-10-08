83b726cf402bb80aea936e9d3240b93f4be36ca3
fuchsde <32522107+fuchsde@users.noreply.github.com>
2018-10-08 19:34:44 +0300

    Update and rename test.cpp to testMultiply.cpp

---
+++ testMultiply.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "function.h"
+using std::cout;
+
+int main (int argc, char *argv[]) 
+{
+  int result = 0;
+    for(int i = 0; i < 10; i++)
+    {
+      for(int j = 0; j < 10; j++)
+      {
+          result = Multiply(i, j);
+          cout << "i: " << i << " * j: " << j " = " << result; 
+          if(result != i*j)
+          {
+            cout << "Testing multiply failed";
+          }
+      }
+    }
+} 
