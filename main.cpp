9fbc2db6c3531befbd8b95d9a051662b2cb7e931
fuchsde <32522107+fuchsde@users.noreply.github.com>
2018-10-08 19:20:53 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main (int argc, char *argv[])
   cout << "Hello World\n";
   cout << "Getting it done\n";
   
-  int result = Math.Multiply(2, 3);
+  int result = Multiply(2, 3);
   cout << "Result: " << result;
   
   return 0;
